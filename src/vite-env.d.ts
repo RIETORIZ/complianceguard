520b73a05a273d97958ea118a7cff5cec255bd00
Knight_dragon_5 R <thedragonryan@gmail.com>
2026-08-01 12:08:58 +0000

    External agent changes

---
+++ src/vite-env.d.ts
@@ -0,0 +1,10 @@
+/// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_BASE44_APP_ID?: string;
+  readonly VITE_BASE44_APP_BASE_URL?: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
